35e1eaf79348c77caf4ffe75b03759abdf0932bd
Sebsatian Pereira <sebasperemald04@gmail.com>
2026-07-23 16:32:48 -0500

    Actualizando estructura html, agregando autores y actualizando estilos - archivos: index.html, style.css y script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const tarjetasVistas = new Set();
-
+ 
 function voltear(cardElement) {
   cardElement.classList.add('volteada');
-
+ 
   if (cardElement.classList.contains('volteada')) {
     if (!tarjetasVistas.has(cardElement)) {
       tarjetasVistas.add(cardElement);
@@ -10,21 +10,21 @@ function voltear(cardElement) {
     }
   }
 }
-
+ 
 function actualizarContador() {
   const contadorDOM = document.getElementById('contador');
   contadorDOM.textContent = tarjetasVistas.size;
 }
-
+ 
 function filtrar(tipo) {
   const cards = document.querySelectorAll('.card');
-
+ 
   cards.forEach(card => {
     if (tipo === 'todas') {
       card.classList.remove('oculto');
     } else if (tipo === 'vocales') {
       const esVocal = card.getAttribute('data-tipo') === 'vocal';
-      
+     
       if (esVocal) {
         card.classList.remove('oculto');
       } else {
